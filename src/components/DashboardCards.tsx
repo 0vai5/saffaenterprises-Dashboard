dbbb4ef6bf6b83832fd17c61752d14208154971b
Ovais Raza <rovais53@gmail.com>
2024-05-22 18:08:45 +0500

    "Refactor DashboardCards component: updated card layout, replaced content, and added table and badge components."

--- src/components/DashboardCards.tsx
+++ src/components/DashboardCards.tsx
@@ -9,55 +9,75 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import Link from "next/link";
+import { Badge } from "./ui/badge";
 
 const DashboardCards = () => {
   return (
-    <div className="flex justify-between items-center gap-5 md:flex-row flex-col w-full mb-10">
-      <Card className="sm:col-span-2 w-full" x-chunk="dashboard-05-chunk-0">
+    <div className="flex justify-evenly items-center gap-5 md:flex-row flex-col w-full mb-10">
+      <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
         <CardHeader className="pb-3">
-          <CardTitle>Your Orders</CardTitle>
+          <CardTitle>Your Invoice</CardTitle>
           <CardDescription className="max-w-lg text-balance leading-relaxed">
             Introducing Our Dynamic Orders Dashboard for Seamless Management and
             Insightful Analysis.
           </CardDescription>
         </CardHeader>
         <CardFooter>
-            <Link href='/invoice'>
-          <Button>Create New Order</Button>
+          <Link href="/invoice">
+            <Button>Create New Invoice</Button>
           </Link>
         </CardFooter>
       </Card>
-      <Card className="w-full" x-chunk="dashboard-05-chunk-1">
+      <Card className="" x-chunk="dashboard-05-chunk-1">
         <CardHeader className="pb-2">
-          <CardDescription>This Week</CardDescription>
-          <CardTitle className="text-4xl">$1,329</CardTitle>
+          <CardDescription>Upcoming Payments</CardDescription>
+          <CardTitle className="text-4xl">$250</CardTitle>
         </CardHeader>
         <CardContent>
           <div className="text-xs text-muted-foreground">
-            +25% from last week
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Customer</TableHead>
+                  <TableHead className="hidden sm:table-cell">Status</TableHead>
+                  <TableHead className="hidden md:table-cell">Date</TableHead>
+                  <TableHead className="text-right">Amount</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow className="bg-accent">
+                  <TableCell>
+                    <div className="font-medium">Liam Johnson</div>
+                    <div className="hidden text-sm text-muted-foreground md:inline">
+                      liam@example.com
+                    </div>
+                  </TableCell>
+                  <TableCell className="hidden sm:table-cell">
+                    <Badge className="text-xs" variant="secondary">
+                      pending
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    2023-06-23
+                  </TableCell>
+                  <TableCell className="text-right">$250.00</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
           </div>
         </CardContent>
-        <CardFooter>
-          <Progress value={25} aria-label="25% increase" />
-        </CardFooter>
       </Card>
-      <Card className="w-full" x-chunk="dashboard-05-chunk-2">
-        <CardHeader className="pb-2">
-          <CardDescription>This Month</CardDescription>
-          <CardTitle className="text-4xl">$5,329</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="text-xs text-muted-foreground">
-            +10% from last month
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Progress value={12} aria-label="12% increase" />
-        </CardFooter>
-      </Card>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default DashboardCards
+export default DashboardCards;
