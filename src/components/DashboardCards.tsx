5edc034d4e875b81f1b70b6d4233e26370725231
Ovais Raza <rovais53@gmail.com>
2024-05-22 18:09:43 +0500

    Remove Progress import from DashboardCards.tsx

--- src/components/DashboardCards.tsx
+++ src/components/DashboardCards.tsx
@@ -8,7 +8,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Progress } from "@/components/ui/progress";
 import {
   Table,
   TableBody,
