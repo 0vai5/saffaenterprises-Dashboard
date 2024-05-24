ef900da1ad1d834b49e6ad4ee4b02f35915255cc
Ovais Raza <129333571+0vai5@users.noreply.github.com>
2024-05-24 18:24:49 +0000

    "Refactor login page to use react-hook-form for form handling and validation"

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,28 @@
+'use client'
+
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-const page = () => {
+type Inputs = {
+  email: string;
+  password: string;
+};
+
+const Page = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  }
+
   return (
     <section className="container flex justify-center items-center min-h-screen">
       <Card className="w-full max-w-sm">
@@ -20,28 +32,35 @@ const page = () => {
             Enter your email below to login to your account.
           </CardDescription>
         </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button className="w-full">Sign in</Button>
-        </CardFooter>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                type="email"
+                {...register('email', {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                    message: "Invalid email address"
+                  }
+                })} />
+              {errors.email && <p>{errors.email.message}</p>}
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input {...register('password', { required: true })} />
+            </div>
+          </CardContent>
+
+          <CardFooter>
+            <Button type="submit" className="w-full">Sign in</Button>
+          </CardFooter>
+        </form>
       </Card>
+
     </section>
   );
 };
 
-export default page;
- 
+export default Page;
