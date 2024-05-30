26edfddbb722a8b77ef2317ed165e4dadaf8ada3
Ovais Raza <129333571+0vai5@users.noreply.github.com>
2024-05-30 11:54:14 +0000

    "Updated Header and MobileNav components to use next/image for logo images"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,28 @@
 import Link from "next/link";
 import NavItems from "./NavItems"
 import MobileNav from "./MobileNav"
+import Image from 'next/image'
 
 const Header = () => {
   return (
     <header className="header">
       <div>
-      <Link
-        href="/"
-      >
-        <img src="./saffaenterprises.png" alt="logo" className="h-32 w-32"/>
-      </Link>
+        <Link
+          href="/"
+        >
+          <Image
+            src={'/saffaenterprises.png'}
+            alt="Company Logo"
+            width={150}
+            height={150}
+          />
+        </Link>
       </div>
       <nav className="hidden md:block">
         <NavItems />
       </nav>
       <MobileNav />
-      
+
     </header>
   );
 };
--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -4,6 +4,7 @@ import {
   Menu
 } from "lucide-react"
 import Link from "next/link";
+import Image from "next/image"
 
 
 const MobileNav = () => {
@@ -12,13 +13,18 @@ const MobileNav = () => {
       <Sheet>
         <SheetTrigger asChild>
           <button className="w-10 h-5 flex items-center justify-center">
-            <Menu className="font-lg text-black "/>
+            <Menu className="font-lg text-black " />
           </button>
         </SheetTrigger>
         <SheetContent className="bg-white drop-shadow-[-100px 0px 30px -10px rgba(0,0,0,0.1)]">
           <div>
             <Link href="/">
-                <img src="/saffaenterprises.png" alt="logo" className="h-32 w-32" />
+              <Image
+                src={'/saffaenterprises.png'}
+                alt="Company Logo"
+                width={150}
+                height={100}
+              />
             </Link>
           </div>
           <NavItems />
