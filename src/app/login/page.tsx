5be65a18c3306b8e4ecd318a468afe9686307cfa
Ovais Raza <129333571+0vai5@users.noreply.github.com>
2024-05-30 12:01:47 +0000

    fixed errors in production

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 type Inputs = {
   email: string;
