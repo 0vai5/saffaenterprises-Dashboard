7cb7128ddca8711c30e4e13f668cb6df9393e4d4
Ovais Raza <129333571+0vai5@users.noreply.github.com>
2024-06-02 06:11:57 +0000

    "Updated findUsers route to ensure database connection and handle errors"

--- src/app/api/users/findUsers/route.ts
+++ src/app/api/users/findUsers/route.ts
@@ -2,26 +2,31 @@ import Connect from '@/DBConfig/DBConfig';
 import User from '@/models/user.model';
 import { NextRequest, NextResponse } from 'next/server';
 
-Connect()
+// Ensure database connection
+Connect();
+
 export async function GET() {
     try {
         const users = await User.find();
-        if (!users) {
+
+        if (!users || users.length === 0) {
             return NextResponse.json({
                 message: 'No users found',
                 status: 404
             });
         }
+
         return NextResponse.json({
             message: 'Users fetched successfully',
             data: users,
             status: 200
         });
     } catch (error) {
+        // Handle errors
         return NextResponse.json({
             message: 'Error fetching users',
             error,
             status: 500
-            });
+        });
     }
 }
