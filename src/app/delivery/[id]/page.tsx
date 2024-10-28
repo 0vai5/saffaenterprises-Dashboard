0d4464e54ba1c518385095ed09389e14cea12e76
Muhammad Ovais Raza <129333571+0vai5@users.noreply.github.com>
2024-10-28 19:16:05 +0500

    Update page.tsx

--- src/app/delivery/[id]/page.tsx
+++ src/app/delivery/[id]/page.tsx
@@ -196,7 +196,7 @@ const Page = ({ params }: any) => {
                 </CardContent>
               </Card>
             </CardContent>
-            <CardFooter className="flex justify-between items-end md:items-center px-5 sm:flex-row flex-col-reverse  gap-[4rem]">
+            <CardFooter className="flex justify-between items-end md:items-center px-6 sm:flex-row flex-col-reverse  gap-[4rem]">
               <div className="flex justify-end items-end flex-col gap-6">
                 <CardDescription>
                   • All the above Items are recieved in good condition.
